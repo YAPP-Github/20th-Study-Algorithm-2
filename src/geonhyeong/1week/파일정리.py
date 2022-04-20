0633d59fc89426f87e7d6fa1ecc32366befed868
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-04-20 17:12:51 +0900

    [Week1] 김건형: 파일정리 설명 추가

--- src/geonhyeong/1week/파일정리.py
+++ src/geonhyeong/1week/파일정리.py
@@ -1,12 +1,14 @@
 # 파일정리.py
 # Question Link: https://www.acmicpc.net/problem/20291
 # Primary idea:     String
-#                   1. 
+#                   1. split으로 '.'을 기준으로 나눈 뒤, 2번째 요소(확장자)를 추출
+#                   2. dictionary에 값을 누적
+#                   3. dictionary key값을 기준으로 오름차순
 # 
-# Time Complexity : O()
-# Space Complexity : O()
-# Runtime: = ms
-# Memory Usage: = MB
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Runtime: 2152 ms
+# Memory Usage: 43.64 MB
 
 dic = dict()
 
