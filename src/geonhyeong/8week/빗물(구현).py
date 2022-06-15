a07ef4b5a7dff4a72c1ad432bd879c98aa260d0f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-15 20:55:52 +0900

    [Week8] 김건형: 블로그, 겹치는 건 싫어, 가장 긴 짝수 연속한 부분 수열(large), 두 용액, 빗물(구현)

--- src/geonhyeong/8week/빗물(구현).py
+++ src/geonhyeong/8week/빗물(구현).py
@@ -5,8 +5,8 @@
 # 
 # Time Complexity : O()
 # Space Complexity : O()
-# Runtime:  ms
-# Memory Usage:  MB
+# Runtime: 68 ms
+# Memory Usage: 30.840 MB
 
 import sys
 input = sys.stdin.readline
@@ -15,9 +15,9 @@ h, w = map(int, input().split())
 block = list(map(int, input().split()))
 
 res = 0
-for i in range(1, w - 1):
-    leftMax = max(block[:i])
-    rightMax = max(block[i+1:])
+for i in range(1, w - 1): # 첫 번째 칸과 마지막칸은 물이 고일수 없음
+    leftMax = max(block[:i]) # 기준에서 가장 높은 왼쪽
+    rightMax = max(block[i+1:]) # 기준에서 가장 높은 오른쪾
 
     compare = min(leftMax, rightMax)
 
