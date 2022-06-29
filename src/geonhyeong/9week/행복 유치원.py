d86d572729e54cf339ca8ce1daddd13374d4fdfc
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-29 15:56:13 +0900

    [Week9] 김건형: 블로그2, 강의실 배정, 행복 유치원, 지구 온난화, 카드 정렬하기

--- src/geonhyeong/9week/행복 유치원.py
+++ src/geonhyeong/9week/행복 유치원.py
@@ -5,8 +5,8 @@
 # 
 # Time Complexity : O()
 # Space Complexity : O()
-# Runtime:  ms
-# Memory Usage:  MB
+# Runtime: 296 ms
+# Memory Usage: 64.404 MB
 
 import sys
 input = sys.stdin.readline
@@ -17,11 +17,11 @@ kids = list(map(int, input().split()))
 
 diff = []
 for i in range(1, len(kids)):
-    diff.append(kids[i] - kids[i - 1])
+    diff.append(kids[i] - kids[i - 1]) # 키 차이
 
-print(diff)
+# print(diff) # [2, 2, 1, 4]
 diff.sort(reverse = True)
-print(diff)
+# print(diff) # [4, 2, 2, 1]
 
-print(diff[k - 1:])
+# print(diff[k - 1:]) # k : 조 갯수
 print(sum(diff[k - 1:]))
